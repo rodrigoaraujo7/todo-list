9aacbca4b615108567dc8e42890f6cd58b982f66
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-08 02:08:47 -0300

    showing the localStorage tasks

--- main.js
+++ main.js
@@ -96,14 +96,53 @@ inputElement.addEventListener('change', () => handleInputChange()) // remove 'er
 
 // (* Local Storage *)
 const updateLocalStorage = () => {
-    const tasks            = tasksContainer.childNodes;
+    // items of the list inside the <div class="tasks-container"></div>
+    const tasks = tasksContainer.childNodes;
 
-    const localStorageTasks = [... tasks].map(task => {
+    const localStorageTasks = [... tasks].map(task => { // for each item
         const content     = task.firstChild;
         const isCompleted = content.classList.contains('completed');
 
+        // creating a JSON data base
         return {description: content.innerText, isCompleted: isCompleted}
     });
 
+    // storing data in a local storage
     localStorage.setItem('tasks', JSON.stringify(localStorageTasks))
 }
+
+// function to recreate the tasks of the localStorage
+const refreshTaskUsingLocalStorage = () => {
+    const tasksFromLocalStorage = JSON.parse(localStorage.getItem('tasks'));
+    console.log({ tasksFromLocalStorage });
+
+    for(const task of tasksFromLocalStorage) {
+        // if validateInput return TRUE
+        const taskItemContainer = document.createElement('div'); // creating new HTML elements
+        taskItemContainer.classList.add('task-item');
+            // (* children elements *)
+
+            // paragraph with content
+            const taskContent     = document.createElement('p');
+            taskContent.innerText = task.description; // receives the input content
+
+            if(task.isCompleted) {
+                taskContent.classList.add('completed')
+            }
+
+            taskContent.addEventListener('click', () => handleClick(taskContent));
+            
+            // delete icon
+            const deleteItem = document.createElement('i'); // font-awesome icon
+            deleteItem.classList.add('fa-solid');
+            deleteItem.classList.add('fa-delete-left');
+
+            deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent));
+
+        taskItemContainer.appendChild(taskContent);
+        taskItemContainer.appendChild(deleteItem);
+
+        tasksContainer.appendChild(taskItemContainer); // container
+    }
+}
+refreshTaskUsingLocalStorage(); // refresh tasks using local storage
